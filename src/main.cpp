954998f0ff11d0e5fe86e379e71cf3276141c5f5
songbingyi <songbingyi@xiaomi.com>
2018-05-30 19:37:34 +0800

    update tab to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-	Mat3b img = Mat3b::zeros(300, 300);
-	imshow("img", img);
-	waitKey();
+    Mat3b img = Mat3b::zeros(300, 300);
+    imshow("img", img);
+    waitKey();
     return 0;
 }
