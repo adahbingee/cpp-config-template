041b05fbe7f3408d3c81437610778b89178e6907
songbingyi <songbingyi@xiaomi.com>
2018-06-04 16:52:51 +0800

    example main code makefile rollback to opencv 3.4.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
 int main(int argc, char *argv[]) {
+    Mat1b img = Mat1b::zeros(300, 300);
+    imwrite("img.png", img + 50);
     return 0;
 }
